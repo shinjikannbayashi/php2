76a54f5893304520fb0488c616dd2f97b84ff7bd
kannbayashi <shinji19965@icloud.com>
2020-01-17 20:50:02 +0900

    k

--- web.php
+++ web.php
@@ -15,17 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 //10章の課題です。
 Route::get('XXX', 'Admin\AAAController@add');
 
-Route::group(['prefix' => 'admin'], function() {
-  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::post('profile/create', 'Admin\ProfileController@create');
+  Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Auth::routes();
